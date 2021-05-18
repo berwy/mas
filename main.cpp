6a6db0ab20574c4efb159dc3ad72503e086b5d88
berwy <>
2021-05-18 05:27:57 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int n, m, mas[1000]; 
+ cout << "Input n (skaitļu skaits masīvā)" << endl;
+ cin >> n; 
+
+
+ cout << "Input m ( n masīva elementus)" << endl;
+ cin >> m; 
+
+for (int i=0; i<n; i++) 
+{
+cout << mas[i] << "  ";
+}
+
+return 0;
+}
